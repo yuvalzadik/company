4287530fbe7533fc4ed88255149a804ecc687b45
Yuval Zadik <YuvalZ@we-ankor.co.il>
2020-10-01 21:21:53 +0300

    added test

--- tests/workerstest.py
+++ tests/workerstest.py
@@ -1,14 +1,16 @@
 import requests
+import sys
 
+api_url = sys.argv[1]
 
 def test_get_workers():
-    workers = requests.get(url="http://localhost:8082/workers")
+    workers = requests.get(url=f"{api_url}/workers")
     if workers.status_code != 200:
         return False
     return True
 
 def test_add_workers():
-    workers = requests.get(url="http://localhost:8082/workers")
+    workers = requests.get(url=f"{api_url}/workers")
     if workers.status_code != 200:
         return False
     return True
