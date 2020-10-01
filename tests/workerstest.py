0293ec0cf1de59a3663188b0feae47e08bc7063c
Yuval Zadik <YuvalZ@we-ankor.co.il>
2020-10-01 20:51:16 +0300

    add new test

--- tests/workerstest.py
+++ tests/workerstest.py
@@ -1,11 +1,17 @@
 import requests
 
 
-def get_workers():
+def test_get_workers():
+    workers = requests.get(url="http://localhost:8082/workers")
+    if workers.status_code != 200:
+        return False
+    return True
+
+def test_add_workers():
     workers = requests.get(url="http://localhost:8082/workers")
     if workers.status_code != 200:
         return False
     return True
 
 
-print(get_workers())
+print(test_get_workers())
